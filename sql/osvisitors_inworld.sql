f7e31fdf4ce5ecf1901b66a98968533657d35d8b
djphil <digitalconcepts@free.fr>
2017-01-21 16:23:53 +0100

    New fields for new options
    New fields for new options

--- sql/osvisitors_inworld.sql
+++ sql/osvisitors_inworld.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 15 Mars 2016 à 15:15
--- Version du serveur :  5.6.21
--- Version de PHP :  5.6.3
+-- Généré le :  Sam 21 Janvier 2017 à 15:47
+-- Version du serveur :  10.1.19-MariaDB
+-- Version de PHP :  5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `opensim`
+-- Base de données :  `owigrid`
 --
 
 -- --------------------------------------------------------
@@ -26,10 +26,14 @@ SET time_zone = "+00:00";
 -- Structure de la table `osvisitors_inworld`
 --
 
-CREATE TABLE IF NOT EXISTS `osvisitors_inworld` (
-`id` int(10) unsigned NOT NULL,
+CREATE TABLE `osvisitors_inworld` (
+  `id` int(10) UNSIGNED NOT NULL,
   `username` varchar(64) NOT NULL,
   `useruuid` varchar(36) NOT NULL,
+  `gender` varchar(8) NOT NULL,
+  `language` varchar(8) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `country` varchar(128) NOT NULL,
   `hostname` varchar(128) NOT NULL,
   `gridname` varchar(128) NOT NULL,
   `regionname` varchar(128) NOT NULL,
@@ -37,21 +41,27 @@ CREATE TABLE IF NOT EXISTS `osvisitors_inworld` (
   `counter` int(11) NOT NULL,
   `firstvisit` int(11) NOT NULL,
   `lastvisit` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Index pour les tables exportées
+--
 
 --
 -- Index pour la table `osvisitors_inworld`
 --
 ALTER TABLE `osvisitors_inworld`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
 
 --
 -- AUTO_INCREMENT pour la table `osvisitors_inworld`
 --
 ALTER TABLE `osvisitors_inworld`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
