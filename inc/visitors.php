14d870a932b498de7cd9c9df62756c6e5a43f7b5
djphil <digitalconcepts@free.fr>
2017-01-21 16:16:10 +0100

    Add new fields in database and securise requests
    Adding new fields ip language and gender in database and Bind value to
    prevent sql injections

--- inc/visitors.php
+++ inc/visitors.php
@@ -6,118 +6,173 @@ if (isset($_POST['terminal']) && !empty($_POST['terminal']))
     {
         if (isset($_POST['name']) && !empty($_POST['name']))
         {
-            if (isset($_POST['object']) && !empty($_POST['object']))
+            if (isset($_POST['uuid']) && !empty($_POST['uuid']))
             {
-                if (isset($_POST['uuid']) && !empty($_POST['uuid']))
+                if (isset($_POST['gender']) && !empty($_POST['gender']))
                 {
-                    if (isset($_POST['region']) && !empty($_POST['region']))
+                    if (isset($_POST['lang']) && !empty($_POST['lang']))
                     {
-                        if (isset($_POST['parcel']) && !empty($_POST['parcel']))
+                        if (isset($_POST['ip']) && !empty($_POST['ip']))
                         {
-                            if (isset($_POST['grid']) && !empty($_POST['grid']))
+                            if (isset($_POST['object']) && !empty($_POST['object']))
                             {
-                                include_once("config.php");
-                                include_once("PDO-mysql.php");
-
-                                $name   = htmlspecialchars($_POST['name']);
-                                $object = htmlspecialchars($_POST['object']);
-                                $uuid   = htmlspecialchars($_POST['uuid']);
-                                $region = htmlspecialchars($_POST['region']);
-                                $parcel = htmlspecialchars($_POST['parcel']);
-                                $grid   = htmlspecialchars($_POST['grid']);
-                                $host   = htmlspecialchars($_POST['host']);
-
-                                if ($name == "") {$name = "Unknow";}
-
-                                echo "Hello  ".$name.", thank you for visiting ".$parcel." on region ".$region;
-
-                                $timestamp  = time();
-                                $hostname   = $host;
-
-                                $query = $db->prepare("
-                                    SELECT id
-                                    FROM osvisitors_inworld
-                                    WHERE (
-                                        useruuid = '".$uuid."'
-                                        AND regionname = '".$region."'
-                                    )
-                                ");
-
-                                $query->execute();
-                                $counter = $query->rowCount();
-
-                                if ($counter == 0)
+                                if (isset($_POST['region']) && !empty($_POST['region']))
                                 {
-                                    $query = $db->prepare("
-                                        INSERT INTO osvisitors_inworld (
-                                            username, 
-                                            useruuid, 
-                                            hostname, 
-                                            gridname, 
-                                            regionname, 
-                                            parcelname, 
-                                            counter, 
-                                            firstvisit,
-                                            lastvisit
-                                        )
-                                        VALUES (
-                                            '".$name."',
-                                            '".$uuid."',
-                                            '".$hostname."',
-                                            '".$grid."', 
-                                            '".$region."',
-                                            '".$parcel."',
-                                            1,
-                                            '".$timestamp."',
-                                            '".$timestamp."'
-                                        )
-                                    ");
-                                    $query->execute();
-                                    exit();
-                                }
+                                    if (isset($_POST['parcel']) && !empty($_POST['parcel']))
+                                    {
+                                        if (isset($_POST['grid']) && !empty($_POST['grid']))
+                                        {
+                                            include_once("config.php");
+                                            include_once("PDO-mysql.php");
 
-                                else
-                                {
-                                    // UPDATE
-                                    $query = $db->prepare("
-                                        UPDATE osvisitors_inworld
-                                        SET counter = counter + 1, 
-                                            hostname = '".$hostname."', 
-                                            gridname = '".$grid."', 
-                                            regionname = '".$region."', 
-                                            parcelname = '".$parcel."', 
-                                            lastvisit = '".$timestamp."'
-                                        WHERE (
-                                            useruuid = '".$uuid."'
-                                            AND
-                                            hostname = '".$hostname."'
-                                            AND
-                                            gridname = '".$grid."'
-                                            AND
-                                            regionname = '".$region."'
-                                            AND
-                                            parcelname = '".$parcel."'
-                                        )
-                                    ");
+                                            $name   = htmlspecialchars($_POST['name']);
+                                            $object = htmlspecialchars($_POST['object']);
+                                            $uuid   = htmlspecialchars($_POST['uuid']);
+                                            $gender = htmlspecialchars($_POST['gender']);
+                                            $lang   = htmlspecialchars($_POST['lang']);
+                                            $ip     = htmlspecialchars($_POST['ip']);
+                                            $region = htmlspecialchars($_POST['region']);
+                                            $parcel = htmlspecialchars($_POST['parcel']);
+                                            $grid   = htmlspecialchars($_POST['grid']);
+                                            $host   = htmlspecialchars($_POST['host']);
 
-                                    $query->execute();
-                                    exit();
+                                            // NOT USED YET BUT SEND BY INWORD SCRIPT
+                                            // $nick    = htmlspecialchars($_POST['nick']);
+
+                                            if ($name == "") {$name = "Unknow";}
+
+                                            echo "Hello  ".$name.", thank you for visiting ".$parcel." on region ".$region;
+
+                                            $timestamp  = time();
+                                            $hostname   = $host;
+
+                                            $query = $db->prepare("
+                                                SELECT id
+                                                FROM osvisitors_inworld
+                                                WHERE (
+                                                    useruuid = ?
+                                                    AND regionname = ?
+                                                )
+                                            ");
+
+                                            $query->bindValue(1, $uuid, PDO::PARAM_STR);
+                                            $query->bindValue(2, $region, PDO::PARAM_STR);
+
+                                            $query->execute();
+                                            $counter = $query->rowCount();
+
+                                            if ($counter == 0)
+                                            {
+                                                $query = $db->prepare("
+                                                    INSERT INTO osvisitors_inworld (
+                                                        username, 
+                                                        useruuid,
+                                                        gender,
+                                                        language,
+                                                        ip,
+                                                        hostname, 
+                                                        gridname, 
+                                                        regionname, 
+                                                        parcelname, 
+                                                        counter, 
+                                                        firstvisit,
+                                                        lastvisit
+                                                    )
+                                                    VALUES (
+                                                        :username,
+                                                        :useruuid,
+                                                        :gender,
+                                                        :language,
+                                                        :ip,
+                                                        :hostname,
+                                                        :gridname, 
+                                                        :regionname,
+                                                        :parcelname,
+                                                        :counter,
+                                                        :firstvisit,
+                                                        :lastvisit
+                                                    )
+                                                ");
+
+                                                $query->bindValue(':username', $name, PDO::PARAM_STR);
+                                                $query->bindValue(':useruuid', $uuid, PDO::PARAM_STR);
+                                                $query->bindValue(':gender', $gender, PDO::PARAM_STR);
+                                                $query->bindValue(':language', $lang, PDO::PARAM_STR);
+                                                $query->bindValue(':ip', $ip, PDO::PARAM_STR);
+                                                $query->bindValue(':hostname', $hostname, PDO::PARAM_STR);
+                                                $query->bindValue(':gridname', $grid, PDO::PARAM_STR);
+                                                $query->bindValue(':regionname', $region, PDO::PARAM_STR);
+                                                $query->bindValue(':parcelname', $parcel, PDO::PARAM_STR);
+                                                $query->bindValue(':counter', '1', PDO::PARAM_INT);
+                                                $query->bindValue(':firstvisit', $timestamp, PDO::PARAM_STR);
+                                                $query->bindValue(':lastvisit', $timestamp, PDO::PARAM_STR);
+
+                                                $query->execute();
+                                                $query = null;
+                                                exit();
+                                            }
+
+                                            else
+                                            {
+                                                // UPDATE
+                                                $query = $db->prepare("
+                                                    UPDATE osvisitors_inworld
+                                                    SET counter = counter + 1, 
+                                                        gender = :gender, 
+                                                        language = :language, 
+                                                        ip = :ip,
+                                                        hostname = :hostname, 
+                                                        gridname = :gridname, 
+                                                        regionname = :regionname, 
+                                                        parcelname = :parcelname, 
+                                                        lastvisit = :lastvisit 
+                                                    WHERE (
+                                                        useruuid = :useruuid 
+                                                        AND
+                                                        hostname = :hostname 
+                                                        AND
+                                                        gridname = :gridname 
+                                                        AND
+                                                        regionname = :regionname 
+                                                        AND
+                                                        parcelname = :parcelname
+                                                    )
+                                                ");
+
+                                                $query->bindValue(':gender', $gender, PDO::PARAM_STR);
+                                                $query->bindValue(':language', $lang, PDO::PARAM_STR);
+                                                $query->bindValue(':ip', $ip, PDO::PARAM_STR);
+                                                $query->bindValue(':hostname', $hostname, PDO::PARAM_STR);
+                                                $query->bindValue(':gridname', $grid, PDO::PARAM_STR);
+                                                $query->bindValue(':regionname', $region, PDO::PARAM_STR);
+                                                $query->bindValue(':parcelname', $parcel, PDO::PARAM_STR);
+                                                $query->bindValue(':lastvisit', $timestamp, PDO::PARAM_STR);
+                                                $query->bindValue(':useruuid', $uuid, PDO::PARAM_STR);
+
+                                                $query->execute();
+                                                $query = null;
+                                                exit();
+                                            }
+                                        }
+                                        else echo "Empty grid detected ...";
+                                    }
+                                    else echo "Empty parcel detected ...";
                                 }
-                                $query = null;
+                                else echo "Empty region detected ...";
                             }
-                            else echo "Empty Grid detected ...";
+                            else echo "Empty object detected ...";
                         }
-                        else echo "Empty Parcel detected ...";
+                        else echo "Empty ip detected ...";
                     }
-                    else echo "Empty Region detected ...";
+                    else echo "Empty language detected ...";
                 }
-                else echo "Empty key detected ...";
+                else echo "Empty gender detected ...";
             }
-            else echo "Empty Object detected ...";
+            else echo "Empty uuid detected ...";
         }
-        else echo "Empty Name detected ...";        
+        else echo "Empty name detected ...";
     }
-    else echo "Invalid Terminal name ...";
+    else echo "Invalid terminal name ...";
 }
 else echo "No direct access ...";
 ?>
